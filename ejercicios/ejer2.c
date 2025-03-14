f64d5da5dae5c45117a3d0a86ffab2a697062381
abyreyes <aby.08reyes@gmail.com>
2025-03-13 23:40:02 -0600

    Create ejer2.c

---
+++ ejercicios/ejer2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int m, n;
+    printf("Ingrese el número de filas: ");
+    scanf("%d", &m);
+    printf("Ingrese el número de columnas: ");
+    scanf("%d", &n);
+
+    int *matrix = (int *)calloc(m * n, sizeof(int));
+    if (matrix == NULL) {
+        printf("Error: No se pudo asignar memoria.\n");
+        return 1;
+    }
+
+    printf("Matriz inicializada con ceros:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i * n + j]);
+        }
+        printf("\n");
+    }
+
+    printf("Ingrese las coordenadas (fila, columna) y el valor para modificar (ej. 1 2 5):\n");
+    int row, col, value;
+    scanf("%d %d %d", &row, &col, &value);
+    matrix[(row-1) * n + (col-1)] = value;
+
+    printf("Matriz actualizada:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i * n + j]);
+        }
+        printf("\n");
+    }
